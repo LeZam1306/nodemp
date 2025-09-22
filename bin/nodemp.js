c5ba36ff32f55186bc2dbc01d5682d1444fcd00f
Lezam <adamzamouricontact@gmail.com>
2025-09-22 23:29:01 +0200

    Initialize nodemp project with basic router functionality and package configuration

---
+++ bin/nodemp.js
@@ -0,0 +1,4 @@
+#!/usr/bin/env node
+import { router } from "../src/index.js";
+
+router(process.argv.slice(2));
---
+++ src/index.js
@@ -0,0 +1,16 @@
+export const router = (arg) => {
+  const command = arg[0];
+  const param = arg[1];
+
+  switch (command) {
+    case "create:component":
+      if (!param) {
+        console.log('The "name" parameter is required');
+      } else {
+      }
+      break;
+    default:
+      console.log("unknown command");
+      break;
+  }
+};
